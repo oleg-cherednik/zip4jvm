e750db8f2ba394bba35dc1aa15ce6da4d6b9dbb5
Oleg Cherednik <oleg.cherednik@gmail.com>
2019-03-04 21:15:45 +0300

    Move EndOfCentralDirectory reader to separate file. Use external lib for detect charset for byte[]

--- src/main/java/net/lingala/zip4j/core/EndOfCentralDirectoryReader.java
+++ src/main/java/net/lingala/zip4j/core/EndOfCentralDirectoryReader.java
@@ -15,35 +15,35 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public final class EndOfCentralDirectoryReader {
 
-    private final LittleEndianRandomAccessFile zip4jRaf;
+    private final LittleEndianRandomAccessFile in;
 
     public EndOfCentralDirectory read() throws ZipException, IOException {
         findHead();
 
         EndOfCentralDirectory dir = new EndOfCentralDirectory();
-        dir.setNoOfThisDisk(zip4jRaf.readShort());
-        dir.setNoOfThisDiskStartOfCentralDir(zip4jRaf.readShort());
-        dir.setTotNoOfEntriesInCentralDirOnThisDisk(zip4jRaf.readShort());
-        dir.setTotNoOfEntriesInCentralDir(zip4jRaf.readShort());
-        dir.setSizeOfCentralDir(zip4jRaf.readInt());
-        dir.setOffsetOfStartOfCentralDir(zip4jRaf.readIntAsLong());
-        dir.setCommentLength(zip4jRaf.readShort());
+        dir.setNoOfThisDisk(in.readShort());
+        dir.setNoOfThisDiskStartOfCentralDir(in.readShort());
+        dir.setTotNoOfEntriesInCentralDirOnThisDisk(in.readShort());
+        dir.setTotNoOfEntriesInCentralDir(in.readShort());
+        dir.setSizeOfCentralDir(in.readInt());
+        dir.setOffsetOfStartOfCentralDir(in.readIntAsLong());
+        dir.setCommentLength(in.readShort());
 
         if (dir.getCommentLength() > 0)
-            dir.setComment(zip4jRaf.readString(dir.getCommentLength()));
+            dir.setComment(in.readString(dir.getCommentLength()));
 
         return dir;
     }
 
     private void findHead() throws ZipException, IOException {
         int commentLength = EndOfCentralDirectory.MAX_COMMENT_LENGTH;
-        long offs = zip4jRaf.length() - EndOfCentralDirectory.MIN_SIZE;
+        long offs = in.length() - EndOfCentralDirectory.MIN_SIZE;
 
         do {
-            zip4jRaf.seek(offs--);
+            in.seek(offs--);
             commentLength--;
 
-            if (zip4jRaf.readInt() == InternalZipConstants.ENDSIG)
+            if (in.readInt() == InternalZipConstants.ENDSIG)
                 return;
         } while (commentLength >= 0 && offs >= 0);
 
