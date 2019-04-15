ed2f2e4bcc6ca53f7d65404c2d7014893a419de5
Oleg Cherednik <oleg.cherednik@gmail.com>
2019-04-15 07:53:14 +0300

    Extract ExtraFieldWriter

--- src/main/java/net/lingala/zip4j/core/writers/CentralDirectoryWriter.java
+++ src/main/java/net/lingala/zip4j/core/writers/CentralDirectoryWriter.java
@@ -3,7 +3,6 @@ package net.lingala.zip4j.core.writers;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import net.lingala.zip4j.io.OutputStreamDecorator;
-import net.lingala.zip4j.model.AESExtraDataRecord;
 import net.lingala.zip4j.model.CentralDirectory;
 import net.lingala.zip4j.model.ZipModel;
 import net.lingala.zip4j.util.InternalZipConstants;
@@ -57,58 +56,8 @@ public final class CentralDirectoryWriter {
         out.writeBytes(fileHeader.getExternalFileAttributes() != null ? fileHeader.getExternalFileAttributes() : new byte[4]);
         out.writeLongAsInt(getOffsLocalFileHeader(fileHeader, writeZip64OffsetLocalHeader));
         out.writeBytes(fileName);
-        writeExtraDataRecord(out, fileHeader, writeZip64FileSize, writeZip64OffsetLocalHeader);
+        new ExtraFieldWriter(fileHeader, zipModel, writeZip64FileSize, writeZip64OffsetLocalHeader).write(out);
         out.writeBytes(fileComment);
-    }
-
-    private void writeExtraDataRecord(OutputStreamDecorator out, CentralDirectory.FileHeader fileHeader, boolean writeZip64FileSize,
-            boolean writeZip64OffsetLocalHeader) throws IOException {
-        if (writeZip64FileSize || writeZip64OffsetLocalHeader)
-            zipModel.zip64();
-
-        writeZip64ExtendedInfo(out, fileHeader, writeZip64FileSize, writeZip64OffsetLocalHeader);
-        writeAesExtraDataRecord(out, fileHeader.getAesExtraDataRecord());
-    }
-
-    private void writeZip64ExtendedInfo(OutputStreamDecorator out, CentralDirectory.FileHeader fileHeader, boolean writeZip64FileSize,
-            boolean writeZip64OffsetLocalHeader) throws IOException {
-        if (!zipModel.isZip64())
-            return;
-
-
-        //Zip64 header
-        out.writeWord((short)InternalZipConstants.EXTRAFIELDZIP64LENGTH);
-
-        //Zip64 extra data record size
-        short dataSize = 0;
-
-        if (writeZip64FileSize)
-            dataSize += 16;
-        if (writeZip64OffsetLocalHeader)
-            dataSize += 8;
-
-        out.writeWord(dataSize);
-
-        if (writeZip64FileSize) {
-            out.writeDword(fileHeader.getUncompressedSize());
-
-            out.writeDword(fileHeader.getCompressedSize());
-        }
-
-        if (writeZip64OffsetLocalHeader)
-            out.writeDword(fileHeader.getOffsLocalFileHeader());
-    }
-
-    private void writeAesExtraDataRecord(OutputStreamDecorator out, AESExtraDataRecord record) throws IOException {
-        if (record == null)
-            return;
-
-        out.writeWord((short)record.getSignature());
-        out.writeWord((short)record.getDataSize());
-        out.writeShort((short)record.getVersionNumber());
-        out.writeBytes(record.getVendor().getBytes(zipModel.getCharset()));
-        out.writeBytes(record.getAesStrength().getValue(), (byte)0);
-        out.writeWord(record.getCompressionMethod().getValue());
     }
 
     private static long getCompressedSize(CentralDirectory.FileHeader fileHeader, boolean writeZip64FileSize) {
