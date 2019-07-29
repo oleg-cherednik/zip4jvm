f91ac64fd02366332e5075699f8521877610fe08
Oleg Cherednik <oleg.cherednik@gmail.com>
2019-07-29 11:14:27 +0300

    Add shouldDecodeStandardEncryptedZip

--- src/main/java/com/cop/zip4j/crypto/pkware/PkwareDecoder.java
+++ src/main/java/com/cop/zip4j/crypto/pkware/PkwareDecoder.java
@@ -15,27 +15,27 @@ public class PkwareDecoder implements Decoder {
     private final LocalFileHeader localFileHeader;
     private final PkwareEngine engine;
 
-    public PkwareDecoder(@NonNull LocalFileHeader localFileHeader, char[] password, byte[] headerBytes) {
+    public PkwareDecoder(@NonNull LocalFileHeader localFileHeader, char[] password, byte[] header) {
         this.localFileHeader = localFileHeader;
         engine = new PkwareEngine(password);
-        init(headerBytes);
+        init(header);
     }
 
-    private void init(byte[] headerBytes) {
+    private void init(byte[] header) {
         long crc32 = localFileHeader.getCrc32();
         // TODO check it, temporary commented
         byte[] crcBuff = new byte[4];
-//        crc32[3] = (byte)(crcBuff[3] & 0xFF);
-//        crc32[2] = (byte)((crcBuff[3] >> 8) & 0xFF);
-//        crc32[1] = (byte)((crcBuff[3] >> 16) & 0xFF);
-//        crc32[0] = (byte)((crcBuff[3] >> 24) & 0xFF);
+        crcBuff[3] = (byte)(crcBuff[3] & 0xFF);
+        crcBuff[2] = (byte)((crcBuff[3] >> 8) & 0xFF);
+        crcBuff[1] = (byte)((crcBuff[3] >> 16) & 0xFF);
+        crcBuff[0] = (byte)((crcBuff[3] >> 24) & 0xFF);
 
-//        if (crc32[2] > 0 || crc32[1] > 0 || crc32[0] > 0)
-//            throw new IllegalStateException("Invalid CRC in File Header");
+        if (crcBuff[2] > 0 || crcBuff[1] > 0 || crcBuff[0] > 0)
+            throw new IllegalStateException("Invalid CRC in File Header");
 
         try {
-            int result = headerBytes[0];
-            for (int i = 0; i < PkwareEncoder.SIZE_HEADER; i++) {
+            int result = header[0];
+            for (int i = 0; i < header.length; i++) {
 //				Commented this as this check cannot always be trusted
 //				New functionality: If there is an error in extracting a password protected file,
 //				"Wrong Password?" text is appended to the exception message
@@ -44,7 +44,7 @@ public class PkwareDecoder implements Decoder {
 
                 engine.updateKeys((byte)(result ^ engine.decrypt()));
                 if (i + 1 != PkwareEncoder.SIZE_HEADER)
-                    result = headerBytes[i + 1];
+                    result = header[i + 1];
             }
         } catch(Exception e) {
             throw new ZipException(e);
