0d1d91dcec10cb50ecfc9f4f530574066a3a38fb
Oleg Cherednik <oleg.cherednik@gmail.com>
2019-10-01 19:49:08 +0300

    Fix code coverage

--- src/test/java/ru/olegcherednik/zip4jvm/model/Zip64Test.java
+++ src/test/java/ru/olegcherednik/zip4jvm/model/Zip64Test.java
@@ -1,8 +1,18 @@
 package ru.olegcherednik.zip4jvm.model;
 
 import org.testng.annotations.Test;
+import ru.olegcherednik.zip4jvm.io.out.DataOutput;
+
+import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.reset;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 /**
  * @author Oleg Cherednik
@@ -26,5 +36,51 @@ public class Zip64Test {
         Zip64 zip64 = Zip64.of(new Zip64.EndCentralDirectoryLocator(), new Zip64.EndCentralDirectory());
         assertThat(zip64.toString()).isNotEqualTo("<null>");
         assertThat(Zip64.NULL.toString()).isEqualTo("<null>");
+    }
+
+    public void shouldRetrieveZeroWhenGetSizeNullObject() {
+        assertThat(Zip64.ExtendedInfo.NULL.getDataSize()).isZero();
+        assertThat(Zip64.ExtendedInfo.NULL.getBlockSize()).isZero();
+    }
+
+    public void shouldRetrieveCorrectStringWhenToString() {
+        assertThat(Zip64.ExtendedInfo.NULL.toString()).isEqualTo("<null>");
+        assertThat(Zip64.ExtendedInfo.builder().disk(1).build().toString()).isNotEqualTo("<null>");
+    }
+
+    public void shouldRetrieveNullObjectWhenAllDataInExtendedInfoNoExist() {
+        assertThat(Zip64.ExtendedInfo.builder().build()).isSameAs(Zip64.ExtendedInfo.NULL);
+    }
+
+    public void shouldIgnoreWriteOutputWhenNullObject() throws IOException {
+        DataOutput out = mock(DataOutput.class);
+
+        Zip64.ExtendedInfo.NULL.write(out);
+
+        verify(out, never()).writeWordSignature(any(int.class));
+    }
+
+    public void shouldIgnoreDataWhenNotExists() throws IOException {
+        DataOutput out = mock(DataOutput.class);
+
+        Zip64.ExtendedInfo.builder().uncompressedSize(1).build().write(out);
+        verify(out, times(1)).writeQword(eq(1L));
+        verify(out, never()).writeDword(any(long.class));
+        reset(out);
+
+        Zip64.ExtendedInfo.builder().compressedSize(2).build().write(out);
+        verify(out, times(1)).writeQword(eq(2L));
+        verify(out, never()).writeDword(any(long.class));
+        reset(out);
+
+        Zip64.ExtendedInfo.builder().localFileHeaderOffs(3).build().write(out);
+        verify(out, times(1)).writeQword(eq(3L));
+        verify(out, never()).writeDword(any(long.class));
+        reset(out);
+
+        Zip64.ExtendedInfo.builder().disk(4).build().write(out);
+        verify(out, never()).writeQword(any(long.class));
+        verify(out, times(1)).writeDword(eq(4L));
+        reset(out);
     }
 }
