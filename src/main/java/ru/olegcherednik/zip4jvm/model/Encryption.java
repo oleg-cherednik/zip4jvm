65e3d453cc2abecce1783f8d7a7f1b4b3f4bbb86
Oleg Cherednik <oleg.cherednik@gmail.com>
2019-10-01 20:02:19 +0300

    Fix code coverage

--- src/main/java/ru/olegcherednik/zip4jvm/model/Encryption.java
+++ src/main/java/ru/olegcherednik/zip4jvm/model/Encryption.java
@@ -2,24 +2,20 @@ package ru.olegcherednik.zip4jvm.model;
 
 import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import ru.olegcherednik.zip4jvm.crypto.Decoder;
 import ru.olegcherednik.zip4jvm.crypto.Encoder;
 import ru.olegcherednik.zip4jvm.crypto.aes.AesDecoder;
 import ru.olegcherednik.zip4jvm.crypto.aes.AesEncoder;
 import ru.olegcherednik.zip4jvm.crypto.aes.AesEngine;
-import ru.olegcherednik.zip4jvm.crypto.aes.AesStrength;
 import ru.olegcherednik.zip4jvm.crypto.pkware.PkwareDecoder;
 import ru.olegcherednik.zip4jvm.crypto.pkware.PkwareEncoder;
-import ru.olegcherednik.zip4jvm.crypto.pkware.PkwareEngine;
 import ru.olegcherednik.zip4jvm.exception.Zip4jvmException;
 import ru.olegcherednik.zip4jvm.io.in.DataInput;
 import ru.olegcherednik.zip4jvm.model.entry.ZipEntry;
 
 import java.io.IOException;
 import java.util.function.Function;
-import java.util.function.LongFunction;
 
 /**
  * @author Oleg Cherednik
@@ -28,30 +24,18 @@ import java.util.function.LongFunction;
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 public enum Encryption {
-    OFF(zipEntry -> Encoder.NULL,
-            (zipEntry, in) -> Decoder.NULL,
-            uncompressedSize -> uncompressedSize,
-            ZipEntry::getChecksum,
-            Compression::getMethod),
-    PKWARE(PkwareEncoder::create, PkwareDecoder::create, PkwareEngine::getCompressedSize, ZipEntry::getChecksum, Compression::getMethod),
-    AES_128(AesEncoder::create, AesDecoder::create,
-            uncompressedSize -> AesEngine.getCompressedSize(uncompressedSize, AesStrength.S128),
-            entry -> 0L, compression -> CompressionMethod.AES),
-    AES_192(AES_128.createEncoder, AES_128.createDecoder,
-            uncompressedSize -> AesEngine.getCompressedSize(uncompressedSize, AesStrength.S192),
-            AES_128.checksum, AES_128.compressionMethod),
-    AES_256(AES_128.createEncoder, AES_128.createDecoder,
-            uncompressedSize -> AesEngine.getCompressedSize(uncompressedSize, AesStrength.S256),
-            AES_128.checksum, AES_128.compressionMethod);
+    OFF(zipEntry -> Encoder.NULL, (zipEntry, in) -> Decoder.NULL, ZipEntry::getChecksum, Compression::getMethod),
+    PKWARE(PkwareEncoder::create, PkwareDecoder::create, ZipEntry::getChecksum, Compression::getMethod),
+    AES_128(AesEncoder::create, AesDecoder::create, entry -> 0L, compression -> CompressionMethod.AES),
+    AES_192(AES_128.createEncoder, AES_128.createDecoder, AES_128.checksum, AES_128.compressionMethod),
+    AES_256(AES_128.createEncoder, AES_128.createDecoder, AES_128.checksum, AES_128.compressionMethod);
 
     private final Function<ZipEntry, Encoder> createEncoder;
     private final CreateDecoder createDecoder;
-    private final LongFunction<Long> expectedCompressedSizeFunc;
     private final Function<ZipEntry, Long> checksum;
     private final Function<Compression, CompressionMethod> compressionMethod;
 
-    @NonNull
-    public static Encryption get(@NonNull ExtraField extraField, @NonNull GeneralPurposeFlag generalPurposeFlag) {
+    public static Encryption get(ExtraField extraField, GeneralPurposeFlag generalPurposeFlag) {
         if (generalPurposeFlag.isStrongEncryption())
             throw new Zip4jvmException("Strong encryption is not supported");
         if (!generalPurposeFlag.isEncrypted())
