326c40557dd23551cfa952c715e482834f8cab2e
Oleg Cherednik <oleg.cherednik@gmail.com>
2019-10-03 11:42:21 +0300

    Fix winrar compatibility test

--- src/test/java/ru/olegcherednik/zip4jvm/compatibility/WinRarToZip4jvmCompatibilityTest.java
+++ src/test/java/ru/olegcherednik/zip4jvm/compatibility/WinRarToZip4jvmCompatibilityTest.java
@@ -14,55 +14,28 @@ import static ru.olegcherednik.zip4jvm.TestData.winRarDeflateSolidZip;
 import static ru.olegcherednik.zip4jvm.TestData.winRarStoreSolidAesZip;
 import static ru.olegcherednik.zip4jvm.TestData.winRarStoreSolidPkwareZip;
 import static ru.olegcherednik.zip4jvm.TestData.winRarStoreSolidZip;
+import static ru.olegcherednik.zip4jvm.TestDataAssert.dirBikesAssert;
+import static ru.olegcherednik.zip4jvm.Zip4jvmSuite.password;
+import static ru.olegcherednik.zip4jvm.assertj.Zip4jvmAssertionsForClassTypes.assertThatDirectory;
 
 /**
  * @author Oleg Cherednik
  * @since 14.08.2019
  */
 @Test
-@SuppressWarnings({ "FieldNamingConvention", "NewClassNamingConvention", "LocalVariableNamingConvention" })
+@SuppressWarnings({ "FieldNamingConvention", "NewClassNamingConvention" })
 public class WinRarToZip4jvmCompatibilityTest {
 
     private static final Path rootDir = Zip4jvmSuite.generateSubDirNameWithTime(WinRarToZip4jvmCompatibilityTest.class);
 
-//    public void zip4jShouldBeReadableForZipInputStream() throws IOException {
-//        Path dstDir = Zip4jSuite.subDirNameAsMethodName(rootDir);
-//        byte[] buf = new byte[1024];
-//
-//        try (ZipInputStream in = new ZipInputStream(new FileInputStream(Zip4jSuite.noSplitZip.toFile()))) {
-//            ZipEntry entry;
-//
-//            while ((entry = in.getNextEntry()) != null) {
-//                Path path = dstDir.resolve(entry.getName());
-//
-//                if (entry.isDirectory())
-//                    Files.createDirectories(path);
-//                else {
-//                    //noinspection NestedTryStatement
-//                    try (BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(path.toFile()))) {
-//                        int len;
-//
-//                        while ((len = in.read(buf)) > 0) {
-//                            out.write(buf, 0, len);
-//                        }
-//                    }
-//                }
-//
-//                in.close();
-//            }
-//        }
-//
-//        assertThatDirectory(dstDir).matches(TestUtils.dirAssert);
-//    }
-
     public void checkCompatibilityWithWinRar() throws IOException {
-        Path parentDir = Zip4jvmSuite.subDirNameAsMethodName(rootDir);
+        Path dir = Zip4jvmSuite.subDirNameAsMethodName(rootDir);
 
-        for (Path zip4jFile : Arrays.asList(winRarStoreSolidZip, winRarStoreSolidPkwareZip, winRarStoreSolidAesZip, winRarDeflateSolidZip,
+        for (Path zip : Arrays.asList(winRarStoreSolidZip, winRarStoreSolidPkwareZip, winRarStoreSolidAesZip, winRarDeflateSolidZip,
                 winRarDeflateSolidPkwareZip, winRarDeflateSolidAesZip)) {
-            Path destDir = Zip4jvmSuite.subDirNameAsRelativePathToRoot(parentDir, zip4jFile);
-            UnzipIt.zip(zip4jFile).destDir(destDir).password(Zip4jvmSuite.password);
-//            assertThatDirectory(destDir).matches(TestDataAssert.starWarsDirAssert);
+            Path destDir = Zip4jvmSuite.subDirNameAsRelativePathToRoot(dir, zip);
+            UnzipIt.zip(zip).destDir(destDir).password(password).extract();
+            assertThatDirectory(destDir).matches(dirBikesAssert);
         }
     }
 
