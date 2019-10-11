9973bc1d873a322f1cc882c876ed18b79085222c
Oleg Cherednik <oleg_cherednik@epam.com>
2019-10-11 18:55:08 +0300

    zip diagnostic

---
+++ src/main/java/ru/olegcherednik/zip4jvm/ZipInfo.java
@@ -0,0 +1,17 @@
+package ru.olegcherednik.zip4jvm;
+
+import lombok.RequiredArgsConstructor;
+
+import java.nio.file.Path;
+
+/**
+ * @author Oleg Cherednik
+ * @since 11.10.2019
+ */
+@RequiredArgsConstructor
+public final class ZipInfo {
+
+    private final Path zip;
+
+
+}
