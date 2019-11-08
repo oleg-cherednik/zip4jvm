cbef9086fca2d9b89059b4537aeacbfed5fb3ec2
Oleg Cherednik <oleg.cherednik@gmail.com>
2019-11-08 09:15:18 +0300

    CompressionMethodViewTest

--- src/main/java/ru/olegcherednik/zip4jvm/model/ExtraField.java
+++ src/main/java/ru/olegcherednik/zip4jvm/model/ExtraField.java
@@ -69,6 +69,26 @@ public final class ExtraField {
         return this == NULL ? "<null>" : ("total: " + getTotalRecords());
     }
 
+
+    /*
+		case 0x0017:
+		{
+			// Strong encryption field.
+			if (archive_le16dec(p + offset) == 2) {
+        unsigned algId =
+                archive_le16dec(p + offset + 2);
+        unsigned bitLen =
+                archive_le16dec(p + offset + 4);
+        int	 flags =
+                archive_le16dec(p + offset + 6);
+        fprintf(stderr, "algId=0x%04x, bitLen=%u, "
+                "flgas=%d\n", algId, bitLen,flags);
+    }
+			break;
+}
+
+     */
+
     public interface Record extends Writer {
 
         int getSignature();
