1c0cd8dfa46fda3b1b42eb47df081df800bcf034
Oleg Cherednik <oleg.cherednik@gmail.com>
2019-12-13 22:04:38 +0300

    Fix Travis CI

--- src/test/java/ru/olegcherednik/zip4jvm/info/ZipInfoTest.java
+++ src/test/java/ru/olegcherednik/zip4jvm/info/ZipInfoTest.java
@@ -1,20 +1,26 @@
 package ru.olegcherednik.zip4jvm.info;
 
+import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import ru.olegcherednik.zip4jvm.TestData;
 import ru.olegcherednik.zip4jvm.Zip4jvmSuite;
 import ru.olegcherednik.zip4jvm.ZipInfo;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import static ru.olegcherednik.zip4jvm.assertj.Zip4jvmAssertions.assertThatFile;
 
 /**
  * @author Oleg Cherednik
  * @since 11.10.2019
  */
+@Slf4j
 @Test
 @SuppressWarnings("FieldNamingConvention")
 public class ZipInfoTest {
@@ -31,16 +37,18 @@ public class ZipInfoTest {
         Zip4jvmSuite.removeDir(rootDir);
     }
 
-//    public void shouldRetrieveInfoWhenStoreSolid() throws IOException {
-//        Path file = Zip4jvmSuite.subDirNameAsMethodName(rootDir).resolve("actual.txt");
-//        Files.createDirectories(file.getParent());
-//
-//        try (PrintStream out = new PrintStream(file.toFile())) {
-//            ZipInfo.zip(TestData.zipStoreSolid).printShortInfo(out);
-//        }
-//
-//        assertThatFile(file).matchesResourceLines("/info/store_solid.txt");
-//    }
+    public void shouldRetrieveInfoWhenStoreSolid() throws IOException {
+        Path file = Zip4jvmSuite.subDirNameAsMethodName(rootDir).resolve("actual.txt");
+        Files.createDirectories(file.getParent());
+
+        log.debug(String.valueOf(Files.size(TestData.zipStoreSolid)));
+
+        try (PrintStream out = new PrintStream(file.toFile())) {
+            ZipInfo.zip(TestData.zipStoreSolid).printShortInfo(out);
+        }
+
+        assertThatFile(file).matchesResourceLines("/info/store_solid.txt");
+    }
 
 //    public void shouldRetrieveInfoWhenStoreSolidPkware() throws IOException {
 //        Path file = Zip4jvmSuite.subDirNameAsMethodName(rootDir).resolve("actual.txt");
@@ -52,8 +60,6 @@ public class ZipInfoTest {
 //
 //        assertThatFile(file).matchesResourceLines("/info/store_solid_pkware.txt");
 //    }
-
-
 
 //    public void shouldRetrieveInfoWhenStoreSolidAes() throws IOException {
 //        Path file = Zip4jvmSuite.subDirNameAsMethodName(rootDir).resolve("actual.txt");
