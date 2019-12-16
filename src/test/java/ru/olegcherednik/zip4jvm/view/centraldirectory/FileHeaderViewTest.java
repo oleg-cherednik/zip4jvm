e088e8958e3defa46a402ab97b95de96afb8ee2b
Oleg Cherednik <oleg.cherednik@gmail.com>
2019-12-16 23:26:55 +0300

    FileHeaderViewTest

--- src/test/java/ru/olegcherednik/zip4jvm/view/centraldirectory/FileHeaderViewTest.java
+++ src/test/java/ru/olegcherednik/zip4jvm/view/centraldirectory/FileHeaderViewTest.java
@@ -16,6 +16,7 @@ import ru.olegcherednik.zip4jvm.model.block.CentralDirectoryBlock;
 import ru.olegcherednik.zip4jvm.model.block.ExtraFieldBlock;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.eq;
@@ -144,6 +145,8 @@ public class FileHeaderViewTest {
                                                             .pos(0)
                                                             .charset(Charsets.UTF_8)
                                                             .position(4, 52).build());
+
+        Arrays.stream(lines).forEach(System.out::println);
 
         assertThat(lines).hasSize(32);
         assertThat(lines[0]).isEqualTo("#1 (PK0102) [UTF-8] ducati-panigale-1199.jpg");
