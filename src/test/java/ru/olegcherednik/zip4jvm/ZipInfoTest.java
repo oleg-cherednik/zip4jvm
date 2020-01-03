d5577bfa6d5838793466f0a8f6eeb17a685a3b9e
Oleg Cherednik <oleg.cherednik@gmail.com>
2020-01-04 02:47:40 +0300

    Decompose STORE split

--- src/test/java/ru/olegcherednik/zip4jvm/ZipInfoTest.java
+++ src/test/java/ru/olegcherednik/zip4jvm/ZipInfoTest.java
@@ -120,6 +120,13 @@ public class ZipInfoTest {
         assertThatDirectory(dir).matchesResourceDirectory("/decompose/store_solid_aes");
     }
 
+    public void shouldDecomposeWhenStoreSplit() throws IOException {
+        Path dir = Zip4jvmSuite.subDirNameAsMethodName(rootDir);
+        Files.createDirectories(dir.getParent());
+        ZipInfo.zip(TestData.zipStoreSplit).decompose(dir);
+        assertThatDirectory(dir).matchesResourceDirectory("/decompose/store_split");
+    }
+
     private static ZipInfo zipInfo() {
         Path path = Paths.get("d:/zip4jvm/tmp/aes.zip");
 //        Files.deleteIfExists(path);
