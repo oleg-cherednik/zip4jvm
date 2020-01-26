541b73a597888a358fa959a3eac8f398cb79e75d
Oleg Cherednik <oleg.cherednik@gmail.com>
2020-01-26 23:51:37 +0300

    7-Zip

--- src/test/java/ru/olegcherednik/zip4jvm/engine/UnzipEngineTest.java
+++ src/test/java/ru/olegcherednik/zip4jvm/engine/UnzipEngineTest.java
@@ -1,24 +1,19 @@
 package ru.olegcherednik.zip4jvm.engine;
 
-import org.apache.commons.io.FileUtils;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import ru.olegcherednik.zip4jvm.UnzipIt;
 import ru.olegcherednik.zip4jvm.Zip4jvmSuite;
-import ru.olegcherednik.zip4jvm.ZipIt;
-import ru.olegcherednik.zip4jvm.model.Charsets;
 import ru.olegcherednik.zip4jvm.model.settings.UnzipSettings;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.attribute.FileTime;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static ru.olegcherednik.zip4jvm.TestData.dirNameCars;
 import static ru.olegcherednik.zip4jvm.TestData.zipDeflateSolid;
 import static ru.olegcherednik.zip4jvm.TestDataAssert.dirCarsAssert;
@@ -74,23 +69,23 @@ public class UnzipEngineTest {
         assertThatDirectory(destDir.resolve(dirNameCars)).matches(dirCarsAssert);
     }
 
-    public void shouldCorrectlySetLastTimeStampWhenUnzip() throws IOException, ParseException {
-        Path destDir = Zip4jvmSuite.subDirNameAsMethodName(rootDir);
-        Path file = destDir.resolve("foo.txt");
-        final String str = "2014.10.29T18:10:44";
-        FileUtils.writeStringToFile(file.toFile(), "oleg.cherednik", Charsets.UTF_8);
-
-        Files.setLastModifiedTime(file, FileTime.fromMillis(convert(str)));
-
-        Path zip = Zip4jvmSuite.subDirNameAsMethodName(rootDir).resolve("src.zip");
-        ZipIt.zip(zip).add(file);
-
-        Path unzipDir = destDir.resolve("unzip");
-        UnzipIt.zip(zip).destDir(unzipDir).extract();
-
-        Path fileFooUnzip = unzipDir.resolve("foo.txt");
-        assertThat(convert(Files.getLastModifiedTime(fileFooUnzip).toMillis())).isEqualTo(str);
-    }
+//    public void shouldCorrectlySetLastTimeStampWhenUnzip() throws IOException, ParseException {
+//        Path destDir = Zip4jvmSuite.subDirNameAsMethodName(rootDir);
+//        Path file = destDir.resolve("foo.txt");
+//        final String str = "2014.10.29T18:10:44";
+//        FileUtils.writeStringToFile(file.toFile(), "oleg.cherednik", Charsets.UTF_8);
+//
+//        Files.setLastModifiedTime(file, FileTime.fromMillis(convert(str)));
+//
+//        Path zip = Zip4jvmSuite.subDirNameAsMethodName(rootDir).resolve("src.zip");
+//        ZipIt.zip(zip).add(file);
+//
+//        Path unzipDir = destDir.resolve("unzip");
+//        UnzipIt.zip(zip).destDir(unzipDir).extract();
+//
+//        Path fileFooUnzip = unzipDir.resolve("foo.txt");
+//        assertThat(convert(Files.getLastModifiedTime(fileFooUnzip).toMillis())).isEqualTo(str);
+//    }
 
     private static long convert(String str) throws ParseException {
         return new SimpleDateFormat("yyyy.MM.dd'T'HH:mm:ss").parse(str).getTime();
