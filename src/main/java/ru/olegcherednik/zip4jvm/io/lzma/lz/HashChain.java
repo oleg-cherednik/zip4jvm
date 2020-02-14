6dd6da663cad88d732b6277235d3b25203267c68
Oleg Cherednik <oleg.cherednik@gmail.com>
2020-02-14 14:31:25 +0300

    lzma - refactoring

--- src/main/java/ru/olegcherednik/zip4jvm/io/lzma/lz/HashChain.java
+++ src/main/java/ru/olegcherednik/zip4jvm/io/lzma/lz/HashChain.java
@@ -35,15 +35,130 @@ final class HashChain extends LzEncoder {
         chain = new int[cyclicSize];
         lzPos = cyclicSize;
 
-        // Substracting 1 because the shortest match that this match
-        // finder can find is 2 bytes, so there's no need to reserve
-        // space for one-byte matches.
+        /*
+         * Substracting 1 because the shortest match that this match finder can find is 2 bytes, so there's no need to reserve space for one-byte
+         * matches.
+         */
         matches = new Matches(properties.getNiceLength() - 1);
 
-        // Use a default depth limit if no other value was specified.
-        // The default is just something based on experimentation;
-        // it's nothing magic.
-        this.depthLimit = properties.getDepthLimit() > 0 ? properties.getDepthLimit() : 4 + properties.getNiceLength() / 4;
+        /*
+         * Use a default depth limit if no other value was specified.
+         * The default is just something based on experimentation; it's nothing magic.
+         */
+        depthLimit = properties.getDepthLimit() > 0 ? properties.getDepthLimit() : 4 + properties.getNiceLength() / 4;
+    }
+
+    @Override
+    public Matches getMatches() {
+        matches.count = 0;
+        int matchLenLimit = MATCH_LEN_MAX;
+        int niceLenLimit = niceLength;
+        int avail = movePos();
+
+        if (avail < matchLenLimit) {
+            if (avail == 0)
+                return matches;
+
+            matchLenLimit = avail;
+
+            if (niceLenLimit > avail)
+                niceLenLimit = avail;
+        }
+
+        hash.calcHashes(buf, pos);
+        int delta2 = lzPos - hash.getHash2Pos();
+        int delta3 = lzPos - hash.getHash3Pos();
+        int currentMatch = hash.getHash4Pos();
+        hash.updateTables(lzPos);
+
+        chain[cyclicPos] = currentMatch;
+
+        int lenBest = 0;
+
+        /*
+         * See if the hash from the first two bytes found a match.
+         * The hashing algorithm guarantees that if the first byte matches, also the second byte does, so there's no need to test the second byte.
+         */
+        if (delta2 < cyclicSize && buf[pos - delta2] == buf[pos]) {
+            lenBest = 2;
+            matches.len[0] = 2;
+            matches.dist[0] = delta2 - 1;
+            matches.count = 1;
+        }
+
+        /*
+         * See if the hash from the first three bytes found a match that is different from the match possibly found by the two-byte hash.
+         * Also here the hashing algorithm guarantees that if the first byte matches, also the next two bytes do.
+         */
+        if (delta2 != delta3 && delta3 < cyclicSize && buf[pos - delta3] == buf[pos]) {
+            lenBest = 3;
+            matches.dist[matches.count++] = delta3 - 1;
+            delta2 = delta3;
+        }
+
+        // If a match was found, see how long it is.
+        if (matches.count > 0) {
+            while (lenBest < matchLenLimit && buf[pos + lenBest - delta2] == buf[pos + lenBest])
+                ++lenBest;
+
+            matches.len[matches.count - 1] = lenBest;
+
+            // Return if it is long enough (niceLen or reached the end of the dictionary).
+            if (lenBest >= niceLenLimit)
+                return matches;
+        }
+
+        // Long enough match wasn't found so easily. Look for better matches from the hash chain.
+        if (lenBest < 3)
+            lenBest = 3;
+
+        int depth = depthLimit;
+
+        while (true) {
+            int delta = lzPos - currentMatch;
+
+            // Return if the search depth limit has been reached or if the distance of the potential match exceeds the dictionary size.
+            if (depth-- == 0 || delta >= cyclicSize)
+                return matches;
+
+            currentMatch = chain[cyclicPos - delta + (delta > cyclicPos ? cyclicSize : 0)];
+
+            /*
+             * Test the first byte and the first new byte that would give us a match that is at least one byte longer than lenBest. This too short
+             * matches get quickly skipped.
+             */
+            if (buf[pos + lenBest - delta] == buf[pos + lenBest] && buf[pos - delta] == buf[pos]) {
+                // Calculate the length of the match.
+                int len = 0;
+                while (++len < matchLenLimit)
+                    if (buf[pos + len - delta] != buf[pos + len])
+                        break;
+
+                // Use the match if and only if it is better than the longest match found so far.
+                if (len > lenBest) {
+                    lenBest = len;
+                    matches.len[matches.count] = len;
+                    matches.dist[matches.count] = delta - 1;
+                    ++matches.count;
+
+                    // Return if it is long enough (niceLen or reached the end of the dictionary).
+                    if (len >= niceLenLimit)
+                        return matches;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void skip(int len) {
+        while (len-- > 0) {
+            if (movePos() != 0) {
+                // Update the hash chain and hash tables.
+                hash.calcHashes(buf, pos);
+                chain[cyclicPos] = hash.getHash4Pos();
+                hash.updateTables(lzPos);
+            }
+        }
     }
 
     /**
@@ -68,126 +183,5 @@ final class HashChain extends LzEncoder {
         }
 
         return avail;
-    }
-
-    @Override
-    public Matches getMatches() {
-        matches.count = 0;
-        int matchLenLimit = MATCH_LEN_MAX;
-        int niceLenLimit = niceLength;
-        int avail = movePos();
-
-        if (avail < matchLenLimit) {
-            if (avail == 0)
-                return matches;
-
-            matchLenLimit = avail;
-            if (niceLenLimit > avail)
-                niceLenLimit = avail;
-        }
-
-        hash.calcHashes(buf, pos);
-        int delta2 = lzPos - hash.getHash2Pos();
-        int delta3 = lzPos - hash.getHash3Pos();
-        int currentMatch = hash.getHash4Pos();
-        hash.updateTables(lzPos);
-
-        chain[cyclicPos] = currentMatch;
-
-        int lenBest = 0;
-
-        // See if the hash from the first two bytes found a match.
-        // The hashing algorithm guarantees that if the first byte
-        // matches, also the second byte does, so there's no need to
-        // test the second byte.
-        if (delta2 < cyclicSize && buf[pos - delta2] == buf[pos]) {
-            lenBest = 2;
-            matches.len[0] = 2;
-            matches.dist[0] = delta2 - 1;
-            matches.count = 1;
-        }
-
-        // See if the hash from the first three bytes found a match that
-        // is different from the match possibly found by the two-byte hash.
-        // Also here the hashing algorithm guarantees that if the first byte
-        // matches, also the next two bytes do.
-        if (delta2 != delta3 && delta3 < cyclicSize
-                && buf[pos - delta3] == buf[pos]) {
-            lenBest = 3;
-            matches.dist[matches.count++] = delta3 - 1;
-            delta2 = delta3;
-        }
-
-        // If a match was found, see how long it is.
-        if (matches.count > 0) {
-            while (lenBest < matchLenLimit && buf[pos + lenBest - delta2]
-                    == buf[pos + lenBest])
-                ++lenBest;
-
-            matches.len[matches.count - 1] = lenBest;
-
-            // Return if it is long enough (niceLen or reached the end of
-            // the dictionary).
-            if (lenBest >= niceLenLimit)
-                return matches;
-        }
-
-        // Long enough match wasn't found so easily. Look for better matches
-        // from the hash chain.
-        if (lenBest < 3)
-            lenBest = 3;
-
-        int depth = depthLimit;
-
-        while (true) {
-            int delta = lzPos - currentMatch;
-
-            // Return if the search depth limit has been reached or
-            // if the distance of the potential match exceeds the
-            // dictionary size.
-            if (depth-- == 0 || delta >= cyclicSize)
-                return matches;
-
-            currentMatch = chain[cyclicPos - delta
-                    + (delta > cyclicPos ? cyclicSize : 0)];
-
-            // Test the first byte and the first new byte that would give us
-            // a match that is at least one byte longer than lenBest. This
-            // too short matches get quickly skipped.
-            if (buf[pos + lenBest - delta] == buf[pos + lenBest]
-                    && buf[pos - delta] == buf[pos]) {
-                // Calculate the length of the match.
-                int len = 0;
-                while (++len < matchLenLimit)
-                    if (buf[pos + len - delta] != buf[pos + len])
-                        break;
-
-                // Use the match if and only if it is better than the longest
-                // match found so far.
-                if (len > lenBest) {
-                    lenBest = len;
-                    matches.len[matches.count] = len;
-                    matches.dist[matches.count] = delta - 1;
-                    ++matches.count;
-
-                    // Return if it is long enough (niceLen or reached the
-                    // end of the dictionary).
-                    if (len >= niceLenLimit)
-                        return matches;
-                }
-            }
-        }
-    }
-
-    @Override
-    public void skip(int len) {
-        while (len-- > 0) {
-            if (movePos() != 0) {
-                // Update the hash chain and hash tables.
-                hash.calcHashes(buf, pos);
-                chain[cyclicPos] = hash.getHash4Pos();
-                hash.updateTables(lzPos);
-            }
-        }
     }
 }
