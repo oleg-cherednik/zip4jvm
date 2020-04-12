52d4317e1258e4cec9d6fde82d3a9c30b4955866
Oleg Cherednik <oleg.cherednik@gmail.com>
2020-04-12 21:51:28 +0300

    bzip2

--- src/main/java/ru/olegcherednik/zip4jvm/io/bzip2/BZip2CompressorInputStream.java
+++ src/main/java/ru/olegcherednik/zip4jvm/io/bzip2/BZip2CompressorInputStream.java
@@ -11,19 +11,7 @@ import java.util.Arrays;
  * @author Oleg Cherednik
  * @since 12.04.2020
  */
-class BZip2CompressorInputStream extends CompressorInputStream {
-
-    private static final int BASEBLOCKSIZE = 100000;
-    private static final int MAX_ALPHA_SIZE = 258;
-    private static final int MAX_CODE_LEN = 23;
-    private static final int RUNA = 0;
-    private static final int RUNB = 1;
-    private static final int N_GROUPS = 6;
-    private static final int G_SIZE = 50;
-    private static final int N_ITERS = 4;
-    private static final int MAX_SELECTORS = (2 + (900000 / G_SIZE));
-    private static final int NUM_OVERSHOOT_BYTES = 20;
-
+class BZip2CompressorInputStream extends CompressorInputStream implements BZip2Constants {
 
     /**
      * Index of the last char in the block, so the block size == last + 1.
