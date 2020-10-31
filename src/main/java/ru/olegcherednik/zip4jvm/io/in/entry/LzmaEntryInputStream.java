84d52a1678a1326104eff4c03ee02ceb64ffc79d
Oleg Cherednik <oleg.cherednik@gmail.com>
2020-11-01 01:41:28 +0300

    Fix bug

--- src/main/java/ru/olegcherednik/zip4jvm/io/in/entry/LzmaEntryInputStream.java
+++ src/main/java/ru/olegcherednik/zip4jvm/io/in/entry/LzmaEntryInputStream.java
@@ -25,8 +25,10 @@ final class LzmaEntryInputStream extends EntryInputStream {
 
     private LzmaInputStream createInputStream() throws IOException {
         in.mark(HEADER);
-        in.skip(1); // major version
-        in.skip(1); // minor version
+        in.readByte();
+        in.readByte();
+//        in.skip(1); // major version
+//        in.skip(1); // minor version
         int headerSize = in.readWord();
 
         if (headerSize != 5)
