b9403627c72a99046f305b9329d7515cdb05bb91
Oleg Cherednik <oleg.cherednik@gmail.com>
2021-11-07 14:55:11 +0300

    zstd - refactoring

--- src/main/java/ru/olegcherednik/zip4jvm/io/zstd/UnsafeUtil.java
+++ src/main/java/ru/olegcherednik/zip4jvm/io/zstd/UnsafeUtil.java
@@ -15,42 +15,11 @@ package ru.olegcherednik.zip4jvm.io.zstd;
 
 import sun.misc.Unsafe;
 
-import java.lang.reflect.Field;
-import java.nio.Buffer;
-import java.nio.ByteOrder;
-
-import static java.lang.String.format;
-
 public final class UnsafeUtil {
 
     public static final int ARRAY_BYTE_BASE_OFFSET = Unsafe.ARRAY_BYTE_BASE_OFFSET;
 
-    private static final Unsafe UNSAFE;
-    private static final long ADDRESS_OFFSET;
-
     private UnsafeUtil() {}
-
-    static {
-        ByteOrder order = ByteOrder.nativeOrder();
-        if (!order.equals(ByteOrder.LITTLE_ENDIAN)) {
-            throw new IncompatibleJvmException(format("Zstandard requires a little endian platform (found %s)", order));
-        }
-
-        try {
-            Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
-            theUnsafe.setAccessible(true);
-            UNSAFE = (Unsafe)theUnsafe.get(null);
-        } catch (Exception e) {
-            throw new IncompatibleJvmException("Zstandard requires access to sun.misc.Unsafe");
-        }
-
-        try {
-            // fetch the address field for direct buffers
-            ADDRESS_OFFSET = UNSAFE.objectFieldOffset(Buffer.class.getDeclaredField("address"));
-        } catch (NoSuchFieldException e) {
-            throw new IncompatibleJvmException("Zstandard requires access to java.nio.Buffer raw address field");
-        }
-    }
 
     public static int getInt(byte[] inputBase, long inputAddress) {
         byte a = inputBase[(int)inputAddress - ARRAY_BYTE_BASE_OFFSET];
@@ -131,8 +100,10 @@ public final class UnsafeUtil {
         outputBase[(int)outputAddress - ARRAY_BYTE_BASE_OFFSET + 3] = (byte)(value & 0xFF);
     }
 
-    public static void copyMemory(Object inputBase, long inputAddress, Object literalsBuffer, long offs, long inputSize) {
-        UNSAFE.copyMemory(inputBase, inputAddress, literalsBuffer, offs, inputSize);
+    public static void copyMemory(Object inputBase, long inputAddress, Object outputBase, long offs, long inputSize) {
+        byte[] in = (byte[])inputBase;
+        byte[] out = (byte[])outputBase;
+        System.arraycopy(in, (int)(inputAddress - ARRAY_BYTE_BASE_OFFSET), out, (int)(offs - ARRAY_BYTE_BASE_OFFSET), (int)inputSize);
     }
 
 }
