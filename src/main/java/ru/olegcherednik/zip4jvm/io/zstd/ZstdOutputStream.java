4c9fe4f7b0b96f0e58602c945587f5dcf2cdc0f3
Oleg Cherednik <oleg.cherednik@gmail.com>
2021-12-06 02:00:10 +0300

    zstd - native implementation

--- src/main/java/ru/olegcherednik/zip4jvm/io/zstd/ZstdOutputStream.java
+++ src/main/java/ru/olegcherednik/zip4jvm/io/zstd/ZstdOutputStream.java
@@ -21,14 +21,14 @@ public class ZstdOutputStream extends OutputStream {
 
     private static int compressionLevel(CompressionLevel compressionLevel) {
         if (compressionLevel == CompressionLevel.SUPER_FAST)
-            return 1;
-        if (compressionLevel == CompressionLevel.FAST)
-            return 2;
-        if (compressionLevel == CompressionLevel.NORMAL)
             return 3;
+        if (compressionLevel == CompressionLevel.FAST)
+            return 5;
+        if (compressionLevel == CompressionLevel.NORMAL)
+            return 11;
         if (compressionLevel == CompressionLevel.MAXIMUM)
             return 17;
-        return 3;
+        return 11;
     }
 
     @Override
