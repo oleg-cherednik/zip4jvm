cd51cf7e2cdd4bbef0e53900bd998055890e7661
Oleg Cherednik <oleg.cherednik@gmail.com>
2022-12-17 14:04:14 +0300

    strong-new - fix

--- src/main/java/ru/olegcherednik/zip4jvm/io/in/data/BufferedDecoderDataInput.java
+++ src/main/java/ru/olegcherednik/zip4jvm/io/in/data/BufferedDecoderDataInput.java
@@ -40,6 +40,7 @@ public final class BufferedDecoderDataInput extends BaseDataInput implements Dec
     private int hi;
 
     private long bytesRead;
+    private boolean eof;
 
     public BufferedDecoderDataInput(DataInput in, Decoder decoder, long bytesTotal) {
         this.in = in;
@@ -86,6 +87,9 @@ public final class BufferedDecoderDataInput extends BaseDataInput implements Dec
 
     @Override
     public int read(byte[] buf, final int offs, int len) throws IOException {
+        if (eof || len < 0)
+            return IOUtils.EOF;
+
         long bytesAvailable = bytesTotal - bytesRead;
         len = (int)Math.min(len, bytesAvailable);
 
@@ -93,13 +97,9 @@ public final class BufferedDecoderDataInput extends BaseDataInput implements Dec
             return IOUtils.EOF;
 
         int res = readFromLocalBuf(buf, offs, len);
-        len = readFromIn(buf, offs + res, len + res);
+        res += readFromIn(buf, offs + res, len + res);
 
-        if (len == IOUtils.EOF)
-            return IOUtils.EOF;
-
-        bytesRead += len;
-        return len == 0 ? 0 : decoder.decrypt(buf, offs, len);
+        return eof ? IOUtils.EOF : res;
     }
 
     private int readFromLocalBuf(byte[] buf, int offs, int len) {
@@ -117,90 +117,17 @@ public final class BufferedDecoderDataInput extends BaseDataInput implements Dec
     }
 
     private int readFromIn(byte[] buf, int offs, int len) throws IOException {
-        return in.read(buf, offs, len);
-    }
+        int res = in.read(buf, offs, len);
 
-
-//    @Override
-//    public int read(byte[] buf, int offs, int len) throws IOException {
-//        long available = getAvailableBytes();
-//
-//        if (available == 0)
-//            return IOUtils.EOF;
-//
-//        len = (int)Math.min(len, available);
-//
-//        if (len <= 0)
-//            return len;
-//
-//        int j = 0;
-//        int a = copyFromLocalBuffer(buf, offs, len);
-//        j += a;
-//        len -= a;
-//
-//        a = directDecrypt(buf, offs + j, len);
-//        j += a;
-//        len -= a;
-//
-//        if (len > 0) {
-//            a = readNextBlockToLocalBuffer();
-//
-//            if (a <= 0)
-//                return a;
-//
-//            a = copyFromLocalBuffer(buf, offs + j, len);
-//            j += a;
-//        }
-//
-//        return j;
-//    }
-
-    private long getAvailableBytes() {
-        return bytesTotal - bytesRead;
-    }
-
-    private int copyFromLocalBuffer(byte[] buf, int offs, int len) {
-        int available = hi - lo;
-
-        if (available == 0)
-            return 0;
-
-        len = Math.min(len, available);
-        System.arraycopy(this.buf, lo, buf, offs, len);
-        lo += len;
-        bytesRead += len;
-
-        if (lo == hi) {
-            lo = 0;
-            hi = 0;
+        if (res == IOUtils.EOF) {
+            eof = true;
+            res = len;
+        } else if (res > 0) {
+            bytesRead += res;
+            res = decoder.decrypt(buf, offs, res);
         }
 
-        return len;
-    }
-
-    private int readNextBlockToLocalBuffer() throws IOException {
-        int a = in.read(buf, 0, blockSize);
-
-        if (a <= 0)
-            return a;
-
-        hi = decoder.decrypt(buf, 0, a);
-        return hi;
-    }
-
-    private int directDecrypt(byte[] buf, int offs, int len) throws IOException {
-        if (len < buf.length)
-            return 0;
-
-        int totalBlocks = len / blockSize;
-        int a = in.read(buf, offs, totalBlocks * blockSize);
-
-        if (a <= 0)
-            return a;
-
-        a = decoder.decrypt(buf, offs, a);
-        bytesRead += a;
-        return a;
+        return res;
     }
 
     @Override
