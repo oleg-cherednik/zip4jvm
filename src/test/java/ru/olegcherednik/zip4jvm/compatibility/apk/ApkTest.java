940b95d889ce24aca4cb4e5870650a9fde006773
Oleg Cherednik <oleg.cherednik@gmail.com>
2023-01-05 00:33:43 +0300

    #67 apk has incorrect extra filed in the entry

--- src/test/java/ru/olegcherednik/zip4jvm/compatibility/apk/ApkTest.java
+++ src/test/java/ru/olegcherednik/zip4jvm/compatibility/apk/ApkTest.java
@@ -1,22 +1,32 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package ru.olegcherednik.zip4jvm.compatibility.apk;
 
 import org.testng.annotations.Test;
-import ru.olegcherednik.zip4jvm.TestDataAssert;
 import ru.olegcherednik.zip4jvm.UnzipIt;
 import ru.olegcherednik.zip4jvm.Zip4jvmSuite;
 import ru.olegcherednik.zip4jvm.assertj.DirectoryAssert;
-import ru.olegcherednik.zip4jvm.assertj.IDirectoryAssert;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.function.Consumer;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
 import static ru.olegcherednik.zip4jvm.TestData.appApk;
-import static ru.olegcherednik.zip4jvm.TestData.fileNameDucati;
-import static ru.olegcherednik.zip4jvm.TestData.fileNameHonda;
-import static ru.olegcherednik.zip4jvm.TestData.fileNameKawasaki;
-import static ru.olegcherednik.zip4jvm.TestData.fileNameSuzuki;
 import static ru.olegcherednik.zip4jvm.assertj.Zip4jvmAssertions.assertThatDirectory;
 
 /**
