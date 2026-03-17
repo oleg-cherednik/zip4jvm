850565fc97cfcac42fbfa98ebed50915c185db08
Oleg Cherednik <oleg.cherednik@gmail.com>
2026-03-17 05:33:31 +0300

    #146 PrintStreamDecorator

---
+++ src/main/java/ru/olegcherednik/zip4jvm/view/PrintStreamDecorator.java
@@ -0,0 +1,64 @@
+package ru.olegcherednik.zip4jvm.view;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.PrintStream;
+import java.util.Locale;
+import java.util.Optional;
+
+/**
+ * @author Oleg Cherednik
+ * @since 11.09.2025
+ */
+@RequiredArgsConstructor
+public class PrintStreamDecorator {
+
+    private final PrintStream out;
+
+    public void format(Locale locale, String format, Object one, Object two) {
+        out.format(Locale.US, format, one, two);
+    }
+
+    public void println() {
+        out.println();
+    }
+
+    public void print(char ch) {
+        out.print(ch);
+    }
+
+    public void println(Object obj) {
+        out.println(obj);
+    }
+
+    public void println(String str, char underscore) {
+        out.println(str);
+
+        for (int i = 0; i < str.length(); i++)
+            out.print(underscore);
+
+        out.println();
+    }
+
+    public void printLine(String format, Object one, Object two, int offs) {
+        one = Optional.ofNullable(one).orElse("");
+        two = Optional.ofNullable(two).orElse("");
+
+        if (offs > 0)
+            one = StringUtils.repeat(" ", offs) + one;
+
+        out.format(Locale.US, format, one, two);
+        out.println();
+    }
+
+    public void printLine(Object one, int offs) {
+        one = Optional.ofNullable(one).orElse("");
+
+        if (offs > 0)
+            one = StringUtils.repeat(" ", offs) + one;
+
+        out.println(one);
+    }
+
+}
