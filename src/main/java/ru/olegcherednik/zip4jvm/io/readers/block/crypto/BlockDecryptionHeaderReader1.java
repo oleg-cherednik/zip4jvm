a1adfd05868e07fe10e6448d74c6ebaa6242101a
Oleg Cherednik <oleg.cherednik@gmail.com>
2026-07-24 17:10:09 +0300

    3des

---
+++ src/main/java/ru/olegcherednik/zip4jvm/io/readers/block/crypto/BlockDecryptionHeaderReader1.java
@@ -0,0 +1,11 @@
+package ru.olegcherednik.zip4jvm.io.readers.block.crypto;
+
+import ru.olegcherednik.zip4jvm.io.readers.DecryptionHeaderReader;
+
+/**
+ * @author Oleg Cherednik
+ * @since 24.07.2026
+ */
+public class BlockDecryptionHeaderReader1 extends DecryptionHeaderReader {
+
+}
